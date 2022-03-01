69b4ddaedc561503d17e47757927f7fb3bea1028
Gael Fraiteur <gael@postsharp.net>
2022-03-01 10:34:53 +0100

    Fixed file header.

--- src/Metalama.Samples.EnumViewModel/Program.cs
+++ src/Metalama.Samples.EnumViewModel/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// This is an open-source Metalama example. See https://github.com/postsharp/Metalama.Samples for more.
 
 using System;
 
