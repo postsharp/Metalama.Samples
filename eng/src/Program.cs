cc1477086df91581d3a15423208dcf7a86b925fa
Gael Fraiteur <gael@postsharp.net>
2026-02-01 08:23:24 +0100

    Update eng.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -12,7 +12,7 @@ using System.IO;
 using System.IO.Compression;
 using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2025_1;
 
-const string dotNetSdkVersion = "9.0.205";
+const string dotNetSdkVersion = "9.0.310";
 
 var product = new Product( MetalamaDependencies.MetalamaSamples )
 {
