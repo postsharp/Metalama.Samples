2c613ddc95d25e26ecd0f9887543d4fb351da2ec
Gael Fraiteur <gael@postsharp.net>
2026-02-01 17:48:46 +0100

    Eng: using preferred .NET versions, updated .gitignore.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -4,6 +4,7 @@
 using Microsoft.Extensions.FileSystemGlobbing;
 using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
@@ -12,21 +13,18 @@ using System.IO;
 using System.IO.Compression;
 using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2026_1;
 
-// For samples we don't use the latest SDK.
-const string dotNetSdkVersion = "10.0.100";
-
 var product = new Product( MetalamaDependencies.MetalamaSamples )
 {
     OverriddenBuildAgentRequirements = new ContainerRequirements( ContainerHostKind.Windows )
     {
         Components =
         [
-            new DotNetComponent( dotNetSdkVersion, DotNetComponentKind.Sdk ),
-            new DotNetComponent( "9.0.205", DotNetComponentKind.Sdk ),
+            new DotNetComponent( PreferredVersions.DotNetSdk.V_10_0, DotNetComponentKind.Sdk ),
+            new DotNetComponent( PreferredVersions.DotNetSdk.V_9_0, DotNetComponentKind.Sdk ),
         ]
     },
     GenerateNuGetConfig = true,
-    DotNetSdkVersion = new DotNetSdkVersion( dotNetSdkVersion ),
+    DotNetSdkVersion = new DotNetSdkVersion( PreferredVersions.DotNetSdk.V_10_0 ),
 
     
     Solutions =
