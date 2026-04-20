46752c936b6fe2bd22ca550b4acf114871e6de97
PostSharpBot <hello+agent@postsharp.net>
2026-04-20 13:51:25 +0200

    Fix memento-2 undo overwriting memento-restored selection (#124)

--- examples/memento/memento-2/MainViewModel.cs
+++ examples/memento/memento-2/MainViewModel.cs
@@ -95,33 +95,7 @@ public sealed partial class MainViewModel
     private void ExecuteUndo()
     {
         this.IsEditing = false;
-
-        // Remember the main list selection status before undo.
-        var item = this.CurrentFish;
-
-        var index =
-            item != null
-                ? (int?) this.Fishes.IndexOf( item )
-                : null;
-
         this._caretaker?.Undo();
-
-        // Fix the current item after undo.
-        if ( index != null )
-        {
-            if ( index < this.Fishes.Count )
-            {
-                this.CurrentFish = this.Fishes[index.Value];
-            }
-            else if ( this.Fishes.Count > 0 )
-            {
-                this.CurrentFish = this.Fishes[^1];
-            }
-            else
-            {
-                this.CurrentFish = null;
-            }
-        }
     }
 
     public bool CanExecuteUndo => this._caretaker?.CanUndo == true;
